0f85c2458d4e319b2dd16f696879bd62997cd01c
erikblomqvist <erik.blomqvist@gmail.com>
2026-05-09 15:04:22 +0200

    Logo massaging

--- src/components/HomeLogo.svelte
+++ src/components/HomeLogo.svelte
@@ -15,30 +15,42 @@
 	.home-title span {
 		--hue: 233;
 		
-		box-sizing: border-box;
-		display: inline-block;
-		
-		aspect-ratio: 1;
-		width: 9rem;
+		position: relative;
+		z-index: 1;
+		isolation: isolate;
+
 		margin-inline-start: 0.3ch;
-		border-radius: 50%;
-		padding-inline: 0.3ch;
-		background-color: hsl(var(--hue) 10% 15%);
-		background-image:
-			radial-gradient(
-				91% 90% at 50% 50%,
-				hsl(var(--hue) 10% 20%) 48%,
-				hsl(var(--hue) 10% 20% / 0) 50%
-			),
-			radial-gradient(
-				88% 88% at 47% 47%,
-				hsl(var(--hue) 10% 85%) 43%,
-				hsl(var(--hue) 10% 85% / 0) 50%
-			),
-			radial-gradient(
-				65% 70% at 40% 60%,
-				hsl(var(--hue) 10% 20%) 46%,
-				hsl(var(--hue) 10% 20% / 0) 50%
-			);
+		
+		&::before {
+			content: "";
+			position: absolute;
+			inset: 0;
+			inset-block-start: 0.05em;
+			inset-inline-start: -0.3em;
+			z-index: -1;
+			box-sizing: border-box;
+			
+			width: 1.5em;
+			height: 1.5em;
+			border-radius: 50%;
+			padding-inline: 0.3ch;
+			background-color: hsl(var(--hue) 10% 15%);
+			background-image:
+				radial-gradient(
+					91% 90% at 50% 50%,
+					hsl(var(--hue) 10% 20%) 48%,
+					hsl(var(--hue) 10% 20% / 0) 50%
+				),
+				radial-gradient(
+					88% 88% at 47% 47%,
+					hsl(var(--hue) 10% 85%) 43%,
+					hsl(var(--hue) 10% 85% / 0) 50%
+				),
+				radial-gradient(
+					65% 70% at 40% 60%,
+					hsl(var(--hue) 10% 20%) 46%,
+					hsl(var(--hue) 10% 20% / 0) 50%
+				);
+		}
 	}
 </style>
