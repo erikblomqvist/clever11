570131062f4d96357e129cd3db583564790caf22
erikblomqvist <erik.blomqvist@gmail.com>
2026-05-14 14:37:29 +0200

    Improve boolean question type AnswerDialog UX/UI

--- src/lib/components/AnswerDialog.svelte
+++ src/lib/components/AnswerDialog.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { _ } from 'svelte-i18n';
 	import { syncDialogOpen } from '$lib/utilities/dialog.js';
+	import { Check, X } from 'lucide-svelte';
 	import Button from './Button.svelte';
 
 	/**
@@ -33,13 +34,17 @@
 	/** @type {HTMLDialogElement | null} */
 	let dialogEl = $state(null);
 	let revealed = $state(false);
+	let resultState = $state(null);
 
 	$effect(() => {
 		syncDialogOpen(dialogEl, open);
 	});
 
 	$effect(() => {
-		if (!open) revealed = false;
+		if (!open) {
+			revealed = false;
+			resultState = null;
+		}
 	});
 
 	const isBoolean = $derived(typeof correctAnswer === 'boolean');
@@ -54,12 +59,25 @@
 			? `--answer-bg: ${/** @type {any} */ (correctAnswer).backgroundColor}`
 			: '',
 	);
+
+	const handleResult = (isCorrect) => {
+		if (resultState) return;
+		resultState = isCorrect ? 'correct' : 'wrong';
+		setTimeout(() => {
+			onresult(isCorrect);
+		}, 1000);
+	};
+
+	const handleBooleanClick = (clickedYes) => {
+		const isCorrect = clickedYes === correctAnswer;
+		handleResult(isCorrect);
+	};
 </script>
 
 <dialog
 	bind:this={dialogEl}
 	class="answer-dialog"
-	class:answer-dialog--boolean={isBoolean && revealed}
+	class:answer-dialog--boolean={isBoolean}
 	class:answer-dialog--color={isColor && revealed}
 	style={`${colorStyle};--seat-rotation:${seatRotation}turn;--rotation-duration-ms:${rotationDurationMs};--rotation-easing:${rotationEasing}`}
 	onkeydown={(e) => e.key === 'Escape' && e.preventDefault()}
@@ -75,32 +93,24 @@
 		<p class="answer-dialog__label">{blobLabel}</p>
 	{/if}
 
-	{#if revealed}
-		{#if isColor}
+	{#if revealed || isBoolean}
+		{#if isColor && revealed}
 			<div class="answer-dialog__color-swatch"></div>
 		{/if}
 
-		<p
-			class="answer-dialog__answer"
-			class:answer-dialog__answer--yes={isBoolean &&
-				correctAnswer === true}
-			class:answer-dialog__answer--no={isBoolean &&
-				correctAnswer === false}
-		>
-			{#if correctAnswer !== null}
-				{#if isBoolean}
-					{correctAnswer
-						? $_('answer_dialog.yes')
-						: $_('answer_dialog.no')}
-				{:else if questionType === 'centuryDecade'}
-					{correctAnswer}-talet
-				{:else if isColor}
-					{/** @type {any} */ (correctAnswer).text}
-				{:else}
-					{correctAnswer}
+		{#if !isBoolean}
+			<div class="answer-dialog__answer">
+				{#if correctAnswer !== null}
+					{#if questionType === 'centuryDecade'}
+						{correctAnswer}-talet
+					{:else if isColor}
+						{/** @type {any} */ (correctAnswer).text}
+					{:else}
+						{correctAnswer}
+					{/if}
 				{/if}
-			{/if}
-		</p>
+			</div>
+		{/if}
 	{:else}
 		<Button
 			class="answer-dialog__reveal-btn"
@@ -111,22 +121,60 @@
 
 	<div
 		class="answer-dialog__actions"
-		class:answer-dialog__actions--visible={revealed}
+		class:answer-dialog__actions--visible={revealed || isBoolean}
 	>
-		<button
-			class="answer-dialog__btn answer-dialog__btn--correct"
-			type="button"
-			onclick={() => onresult(true)}
-		>
-			{$_('answer_dialog.correct')}
-		</button>
-		<button
-			class="answer-dialog__btn answer-dialog__btn--wrong"
-			type="button"
-			onclick={() => onresult(false)}
-		>
-			{$_('answer_dialog.wrong')}
-		</button>
+		{#if resultState}
+			<div class="answer-dialog__result">
+				<div
+					class="answer-dialog__result-icon-wrapper"
+					class:answer-dialog__result-icon-wrapper--correct={resultState ===
+						'correct'}
+					class:answer-dialog__result-icon-wrapper--wrong={resultState ===
+						'wrong'}
+				>
+					{#if resultState === 'correct'}
+						<Check size={32} />
+					{:else}
+						<X size={32} />
+					{/if}
+				</div>
+				<p class="answer-dialog__result-text">
+					{resultState === 'correct'
+						? $_('answer_dialog.correct')
+						: $_('answer_dialog.wrong')}
+				</p>
+			</div>
+		{:else if isBoolean}
+			<button
+				class="answer-dialog__btn answer-dialog__btn--wrong"
+				type="button"
+				onclick={() => handleBooleanClick(false)}
+			>
+				<X size={28} />
+			</button>
+			<button
+				class="answer-dialog__btn answer-dialog__btn--correct"
+				type="button"
+				onclick={() => handleBooleanClick(true)}
+			>
+				<Check size={28} />
+			</button>
+		{:else}
+			<button
+				class="answer-dialog__btn answer-dialog__btn--wrong"
+				type="button"
+				onclick={() => onresult(false)}
+			>
+				{$_('answer_dialog.wrong')}
+			</button>
+			<button
+				class="answer-dialog__btn answer-dialog__btn--correct"
+				type="button"
+				onclick={() => onresult(true)}
+			>
+				{$_('answer_dialog.correct')}
+			</button>
+		{/if}
 	</div>
 </dialog>
 
@@ -198,14 +246,9 @@
 		font-size: clamp(2rem, 10vw, 3.5rem);
 		font-weight: 700;
 		line-height: 1.05;
-	}
-
-	.answer-dialog__answer--yes {
-		color: hsl(114 62% 38%);
-	}
-
-	.answer-dialog__answer--no {
-		color: hsl(0 86% 48%);
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 
 	.answer-dialog :global(.answer-dialog__reveal-btn) {
@@ -243,6 +286,9 @@
 		font-weight: 600;
 		cursor: pointer;
 		transition: background-color 0.15s;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.answer-dialog__btn--correct {
@@ -261,5 +307,50 @@
 
 	.answer-dialog__btn--wrong:hover {
 		background-color: hsl(0 86% 44%);
+	}
+
+	.answer-dialog__result {
+		grid-column: span 2;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 0.5rem;
+		animation: result-pop 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+
+	@keyframes result-pop {
+		from {
+			opacity: 0;
+			transform: scale(0.5);
+		}
+		to {
+			opacity: 1;
+			transform: scale(1);
+		}
+	}
+
+	.answer-dialog__result-icon-wrapper {
+		display: grid;
+		place-items: center;
+		width: 3.5rem;
+		height: 3.5rem;
+		border-radius: 50%;
+		color: var(--white);
+	}
+
+	.answer-dialog__result-icon-wrapper--correct {
+		background-color: hsl(114 62% 43%);
+	}
+
+	.answer-dialog__result-icon-wrapper--wrong {
+		background-color: hsl(0 86% 53%);
+	}
+
+	.answer-dialog__result-text {
+		margin: 0;
+		font-family: var(--font-family-display);
+		font-size: var(--font-size-md);
+		font-weight: 700;
+		color: var(--grayscale-900);
 	}
 </style>
